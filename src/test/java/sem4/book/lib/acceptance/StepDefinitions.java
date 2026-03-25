f113da909ca662b50a2bab55b94711d43312f04e
Ellin2024 <ellinyumiko@gmail.com>
2026-03-25 15:20:48 +0630

    updated 3:20PM

--- src/test/java/sem4/book/lib/acceptance/StepDefinitions.java
+++ src/test/java/sem4/book/lib/acceptance/StepDefinitions.java
@@ -4,24 +4,41 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import sem4.book.lib.dto.BookRequestDto;
 import sem4.book.lib.dto.BookResponseDto;
+import sem4.book.lib.dto.CategoryRequestDto;
+import sem4.book.lib.dto.CategoryResponseDto;
 import sem4.book.lib.service.BookService;
+import sem4.book.lib.service.CategoryService;
 
 public class StepDefinitions 
 {
     @Autowired
     private BookService bookService;
 
+    @Autowired
+    private CategoryService categoryService;
+    
     private BookRequestDto request;
     private BookResponseDto response;
-
+    
+    private CategoryResponseDto createCategory;
+    
+    @Before
+    public void stepUp()
+    {
+    	createCategory = categoryService.create(new CategoryRequestDto("Harry Poter","Magician",null));
+    }
+    
     @Given("Book {string} by {string} with ISBN number {string}")
     public void createBook(String title, String author, String isbn) 
     {
+    	
+    	
 
     	
         request = BookRequestDto.builder()
